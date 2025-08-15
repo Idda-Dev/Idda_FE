255cc522dbc33065ecf7e44304a459987fdbd492
choidaye8170 <gracechoidaye@gmail.com>
2025-08-16 02:04:47 +0900

    feat : calendar 날짜클릭시 캘린더 크기조절 기능 구현

--- src/pages/MissionPage.jsx
+++ src/pages/MissionPage.jsx
@@ -1,3 +1,4 @@
+// MissonPage.jsx
 import React, { useState } from "react";
 import styled from "styled-components";
 
@@ -7,11 +8,13 @@ import TabBar from "../components/TabBar.jsx";
 import PupleMissionIcon from "../assets/PupleMissionIcon.png";
 import PrevIcon from "../features/MissonCalendar/assets/PrevIcon.png";
 import NextIcon from "../features/MissonCalendar/assets/NextIcon.png";
+import Photo from "../features/PostItem/assets/ProfileIcon.png";
 
-
+// ui보려고 캘린더 크기조절되는거 걍 과거현재미래로 나눠둠 !
 
 const MissonPage = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [selectedDateType, setSelectedDateType] = useState(null);
 
   const handlePrevMonth = () => {
     const year = currentDate.getFullYear();
@@ -26,6 +29,12 @@ const MissonPage = () => {
   };
 
   const monthLabel = `${currentDate.getMonth() + 1}월`;
+
+  const handleDateClick = (day, type) => {
+    setSelectedDateType(type);
+  };
+
+  const isPast = selectedDateType === "past";
 
   return (
     <Container>
@@ -42,28 +51,53 @@ const MissonPage = () => {
           </NextButton>
         </Wrapper>
 
-        <CalendarBox>
+        <CalendarBox isPast={isPast}>
           <Calendar
             year={currentDate.getFullYear()}
             month={currentDate.getMonth()}
+            onDateClick={handleDateClick}
+            hide={isPast} // 글씨와 원 크기 제어
           />
         </CalendarBox>
-        <Massege>한 걸음, 두 걸음, 같이 걸어요.</Massege>
+
+        {isPast && (
+          <PhotoBox isPast={isPast}>
+            <img
+              src={Photo}
+              alt="미션 사진"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </PhotoBox>
+        )}
+
+        {selectedDateType === "future" && (
+          <Modal>
+            <div>아직 미션이 없어요!</div>
+          </Modal>
+        )}
+
+        {!isPast && <Massege>한 걸음, 두 걸음, 같이 걸어요.</Massege>}
       </ContentWrapper>
 
-      <TabBar icons={{ mission : PupleMissionIcon }} />
+      <TabBar icons={{ mission: PupleMissionIcon }} />
     </Container>
   );
 };
 
+export default MissonPage;
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  position: relative; 
+  position: relative;
 `;
 
 const ContentWrapper = styled.div`
-  height: calc(100% - 2.5rem); /* 탭바 높이 제외 */
+  height: calc(100% - 2.5rem);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -85,7 +119,7 @@ const MonthBox = styled.div`
   width: 35%;
   aspect-ratio: 55 / 13;
   border-radius: 36px;
-  background-color: #B1AAFF;
+  background-color: #b1aaff;
   background-repeat: no-repeat;
   background-position: center;
   background-size: contain;
@@ -94,10 +128,10 @@ const MonthBox = styled.div`
 
 const MonthLabel = styled.div`
   position: absolute;
-  top: 50%; 
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  font-size: 0.8rem;
+  font-size: 0.9rem;
   color: black;
   pointer-events: none;
 `;
@@ -118,41 +152,51 @@ const ArrowButton = styled.button`
     outline: none;
     box-shadow: none;
   }
-
 `;
 
-
-
 const Arrow = styled.img`
-  width: 70%;       /* 버튼 안에서 비율 조절 */
+  width: 70%;
   height: 70%;
   object-fit: contain;
 `;
 
-
-const PrevButton = styled(ArrowButton)`
-  background-color: transparent;
-`;
-
-const NextButton = styled(ArrowButton)`
-  background-color: transparent;
-`;
+const PrevButton = styled(ArrowButton)``;
+const NextButton = styled(ArrowButton)``;
 
 const CalendarBox = styled.div`
   padding: 3rem;
-  width: 100%;
+  width: ${({ isPast }) => (isPast ? "90%" : "100%")};
   max-width: 480px;
-  height: 30rem;
+  height: ${({ isPast }) => (isPast ? "21rem" : "30rem")};
   display: flex;
   justify-content: center;
   background-color: white;
+`;
+
+const PhotoBox = styled.div`
+  width: ${({ isPast }) => (isPast ? "70%" : "100%")};
+  height: 10rem;
+  background-color: #eee;
+  margin-top: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Massege = styled.p`
   font-size: 9px;
   color: #444444;
 `;
-
-
-
-export default MissonPage;
