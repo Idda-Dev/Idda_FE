a1b708c482ba2106781e5abf4e1ffc2dc625d6e6
choidaye8170 <gracechoidaye@gmail.com>
2025-08-17 17:15:25 +0900

    feat: ShopPage 관련 컴포넌트 파일 생성

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import ShopHeader from "../features/Shop/components/ShopHeader";
+import TabBar from "../components/TabBar";
+import Box from "../features/Shop/components/Box";
+import ShopList from "../features/Shop/components/ShopList";
+import styled from "styled-components";
 
 const ShopPage = () => {
-  return <div>상점페이지입니다</div>;
+  return (
+    <Wrapper>
+      <ShopHeader/>
+      <Box/>
+      <ShopList/>
+      <TabBar/>
+    </Wrapper>
+  );
 };
 
 export default ShopPage;
+
+const Wrapper = styled.div`
+  width: 100%;
+`
