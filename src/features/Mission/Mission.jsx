2849435d2e4347ef38dc8e23bf6ee2be04112a30
sunwoo-h <flexible8011@gmail.com>
2025-08-19 20:34:02 +0900

    feat: 미션 카드 ui 구현

--- src/features/Mission/Mission.jsx
+++ src/features/Mission/Mission.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Star1 from "./assets/Star1.png";
 import CalendarIcon from "./assets/CalendarIcon.png";
-import Card from "./assets/Card.png";
+import MissionCard from "./assets/MissionCard.png";
 import { Row } from "../../components/CommonComponents";
 import { useState, useRef } from "react";
 import ReverseCard from "./assets/ReverseCard.png";
@@ -39,15 +39,11 @@ const Mission = () => {
         />
       </Row>
       <ImageWrapper>
-        <BackgroundImage src={Card} alt="카드" />
+        <BackgroundImage src={MissionCard} alt="카드" />
         <TextOverlayBold>동네 골목길 10분 걷기</TextOverlayBold>
-        <TextOverlayRegular>중간에 돌아와도 괜찮아요.</TextOverlayRegular>
-        <TextOverlayRegular style={{ top: "48%" }}>
-          중요한 건 오늘도 나왔다는 거에요.
-        </TextOverlayRegular>
-        <CandyOverlay>사탕 5개</CandyOverlay>
+        <TextOverlayRegular>{`중간에 돌아와도 괜찮아요. \n중요한 건 오늘도 나왔다는 거에요`}</TextOverlayRegular>
+        <CandyOverlay>5개</CandyOverlay>
       </ImageWrapper>
-
       <Row style={{ justifyContent: "space-between", marginBottom: "20px" }}>
         <Row style={{ gap: "10px" }}>
           <img src={Star1} alt="Star" style={{ width: "10%" }} />
@@ -125,22 +121,23 @@ const BackgroundImage2 = styled.img`
 
 const TextOverlayBold = styled.p`
   position: absolute;
-  top: 20%;
+  top: 25%;
   left: 50%;
   transform: translate(-50%, -50%); /* 정확히 중앙으로 이동 */
-  color: #000;
+  color: #fff;
   font-size: 16px;
   font-weight: 600;
 `;
 
 const TextOverlayRegular = styled.p`
   position: absolute;
-  top: 40%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%); /* 정확히 중앙으로 이동 */
-  color: #000;
+  color: #c4c4c4;
   font-size: 10px;
   font-weight: 400;
+  white-space: pre-line;
 `;
 
 const CandyOverlay = styled.div`
@@ -148,7 +145,7 @@ const CandyOverlay = styled.div`
   height: 30px;
   position: absolute;
   top: 75%;
-  left: 67.5%;
+  left: 78%;
   font-size: 10px;
   font-weight: 400;
 `;
