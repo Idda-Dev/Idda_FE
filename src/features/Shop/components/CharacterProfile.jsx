7d4c785a16a9938f8be247b3e4120ca1bed51291
choidaye8170 <gracechoidaye@gmail.com>
2025-08-21 18:19:41 +0900

    feat : 쿠폰페이지 프로필 api 연동 코드 작성

--- src/features/Shop/components/CharacterProfile.jsx
+++ src/features/Shop/components/CharacterProfile.jsx
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from "axios";
 import CharacterIcon from "../assets/Photo.png";
 import Candy from "../assets/Candy.png";
+import { userinfo } from "../../../mocks/userinfo";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const CharacterProfile = () => {
-  const navigate = useNavigate(); // 페이지 이동 함수
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const userId = 1;
 
   const goToCoupon = () => {
-    navigate("/coupon"); // /coupon 페이지로 이동
+    navigate("/coupon");
   };
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        if (!BASE_URL) {
+          console.warn("⚠️ BASE_URL이 설정되지 않았습니다. → 목데이터 사용");
+          setUserInfo(userinfo);
+          return;
+        }
+
+        const res = await axios.get(`${BASE_URL}/api/users/${userId}`);
+        setUserInfo(res.data);
+      } catch (err) {
+        console.error("API 호출 실패:", err);
+        setUserInfo(userinfo);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserInfo();
+  }, [userId]); 
+
+  if (loading) {
+    return <div>로딩중...</div>;
+  }
+  
+  if (!userInfo) {
+      return <div>사용자 정보를 불러올 수 없습니다.</div>;
+  }
 
   return (
     <Container>
-      <Profile src={CharacterIcon} />
+      <Profile src={userInfo.profileImageUrl} />
       <Content>
         <Wrapper>
           <Icon src={Candy} />
-          <MyCandy>보유 솜사탕 40개</MyCandy>
+          <MyCandy>보유 솜사탕 {userInfo.candy}개</MyCandy>
         </Wrapper>
         <Button onClick={goToCoupon}>내 쿠폰함 가기</Button>
       </Content>
@@ -82,8 +120,8 @@ const Button = styled.button`
   font-size: 0.6rem;
   font-weight: 550;
   cursor: pointer;
-  width: 70%;                
-  height: 1.5rem;         
+  width: 70%; 
+  height: 1.5rem; 
   text-align: center;
   display: flex;
   justify-content: center;
