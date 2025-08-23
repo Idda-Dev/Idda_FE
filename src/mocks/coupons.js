45b143e6d18705cfbfccef001e9ec0e44184e956
choidaye8170 <gracechoidaye@gmail.com>
2025-08-23 18:40:28 +0900

    fix : 쿠폰함 api 가게 이미지 연동 오류 해결

--- src/mocks/coupons.js
+++ src/mocks/coupons.js
@@ -5,6 +5,7 @@ export const coupons = [
         "couponId": 2,
         "storeName": "버거킹 신대방점",
         "title": "와퍼세트 20% 할인",
+        "storeImageUrl":null,
         "price": 150,
         "maxCount": 100,
         "issuedCount": 0
@@ -13,6 +14,7 @@ export const coupons = [
         "couponId": 1,
         "storeName": "카페 모카하우스",
         "title": "아메리카노 1잔 무료",
+        "storeImageUrl":null,
         "price": 50,
         "maxCount": 100,
         "issuedCount": 2
