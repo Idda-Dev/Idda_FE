f7a2ffd5de2dc3d197ee2f00e454f2ab31f6758b
choidaye8170 <gracechoidaye@gmail.com>
2025-09-17 14:41:45 +0900

    feat : 서비스설명 페이지 1차 퍼블리싱 완료

--- src/features/ServiceInfo/pages/FirstPage.jsx
+++ src/features/ServiceInfo/pages/FirstPage.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import styled from 'styled-components';
+import StarIcon from "../assets/Star.png"
 
 const FirstPage = () => {
   return (
-    <div>FirstPage</div>
+    <Container>
+        <Star src={StarIcon} alt="Star" />
+        <TextWrapper>
+            <Text1>성공한 미션들이 쌓이면 다음 뭉치가 될 수 있어요.</Text1>
+            <Text2>한뭉치 - 두뭉치 - 세뭉치 - 네뭉치</Text2>
+        </TextWrapper>
+        <Wrapper>
+            <Box>한뭉치는 방안에서 할 수 있는 미션부터 시작해요</Box>
+            <Box>두뭉치는 집안에서 할 수 있는 미션부터 시작해요</Box>
+            <Box>세뭉치는 작은 외출 미션부터 시작해요</Box>
+            <Box>네뭉치는 타인과 상호작용 미션부터 시작해요</Box>
+        </Wrapper>
+        <TextWrapper>
+            <Text2>마지막 단계는 솜뭉치에요.</Text2>
+            <Text3>
+                모든 유형의 미션을 받으며<br />
+                이불 밖으로 나가보아요 :
+            </Text3>
+        </TextWrapper>
+    </Container> 
   )
 }
 
-export default FirstPage
+export default FirstPage;
+
+const Container = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+
+    background-color: #ECEAFF;
+`
+
+const Star=styled.img`
+    width: 1.4rem;
+    object-fit: cover;
+`
+
+const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 20rem;
+    height: 13rem;
+`
+
+const Box=styled.div`
+    background-color: white;
+    padding: 0.5rem 0;
+    border-radius: 36px;
+    font-size: 0.9rem;
+    font-weight: 550;
+`
+
+const Text1 = styled.p`
+    margin: 0;
+    font-size: 0.8rem;
+`
+
+const Text2 = styled.p`
+    margin: 0;
+    font-size: 1.2rem;
+`
+
+const Text3 = styled.p`
+    margin: 0;
+    font-size: 0.9rem;
+`
