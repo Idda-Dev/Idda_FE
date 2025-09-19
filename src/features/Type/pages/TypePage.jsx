a9c3c10a9c553eb993f15ce83dd280bd57ca78b2
choidaye8170 <gracechoidaye@gmail.com>
2025-09-19 13:25:52 +0900

    feat : 집콕유형 페이지 퍼블리싱 완료

--- src/features/Type/pages/TypePage.jsx
+++ src/features/Type/pages/TypePage.jsx
@@ -1,46 +1,123 @@
 import React from 'react'
 import styled from 'styled-components'
-import ProfileIcon from "../assets/ProfileIcon.png"
+import { useNavigate } from 'react-router-dom'
+
+import HanmoongchiGif from "../assets/Hanmoongchi.gif"
+import HomeIcon from "../assets/HomeIcon.png"
+import NextBt from "../assets/NextBt.png"
 
 const TypePage = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-        <Icon src={ProfileIcon}/>
-        <Text1>김연희님의 집콕 유형은 한뭉치에요.</Text1>
-        <Text1>네뭉치는 집콕력이 보통이에요.</Text1>
-        <Text1>취미 생활을 위한 외출은 가능하지만, <br/>타인과의 상호작용은 적어요</Text1>
-        <Box>네뭉치는 타인과 상호작용하는 미션부터 시작해요</Box>
-        <Text1>성공한 미션들이 쌓이면<br/>다음 뭉치가 될 수 있어요.</Text1>
-        <Text1>마지막 단계는 솜뭉치에요.</Text1>
-        <Text1>모든 유형의 미션을 받으며<br/>이불 밖으로 나가보여요 :)</Text1>
+      <Wrapper>
+        <Icon src={HomeIcon}/>
+        <Text1>집콕 유형 테스트 결과</Text1>
+      </Wrapper>
+      <Box>
+        <Gif src={HanmoongchiGif} alt="결과 GIF"/>
+        <Type>한뭉치</Type>
+      </Box>
+      <Text2>
+        연희님의 집콕 유형은 <Highlight>한뭉치</Highlight> 에요.
+      </Text2>
+      <Text3>한뭉치는 집콕력이 아주 높아요.</Text3>
+      <Text4>방안에서 할 수 있는 미션부터 시작해요!</Text4>
+      <NextButton 
+        src={NextBt} 
+        alt="Next" 
+        onClick={() => navigate("/serviceInfo1")} 
+      />
     </Container>
   )
 }
 
 export default TypePage;
 
-const Container=styled.div`
-    background-color: #ECEAFF;
-    width: 100%;
-    height: 100%;
-    padding-bottom: 5rem;
-    padding-top: 7rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
+const Container = styled.div`
+  background-color: #ECEAFF;
+  width: 100%;
+  height: 100vh; /* 화면 전체 높이 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
 `
 
-const Icon=styled.img`
-    width: 6rem;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+  width: 100%;
+  padding: 2rem;
+  gap: 0.7rem;
 `
 
-const Text1=styled.p``
-
-const Box=styled.div`
-    background-color: white;
-    font-size: 0.9rem;
-    padding: 0.4rem 0.8rem;
-    border-radius: 59px;
+const Icon = styled.img`
+  height: 70%;
 `
 
+const Box = styled.div`
+  background-color: white;
+  width: 100%;
+  height: 25rem;
+  padding: 2rem 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Gif = styled.img`
+  height: 17rem;
+`
+
+const Type = styled.p`
+  background-color: #D1CDFF;
+  margin: 0;
+  padding: 0.3rem 0.6rem;
+  border-radius: 36px;
+  font-size: 1rem;
+`
+
+const NextButton = styled.img`
+  width: 1.9rem;
+  position: absolute;
+  bottom: 1.4rem;
+  cursor: pointer;
+`
+
+const Text1 = styled.p`
+  margin: 0;
+  color: #2F0047;
+  font-size: 1.2rem;
+`
+
+const Text2 = styled.p`
+  margin: 3rem 0 0 0;
+  font-size: 1rem;
+  color: black;
+`
+
+const Highlight = styled.span`
+  background-color: white;
+  border-radius: 36px;
+  padding: 0.3rem;
+`
+
+const Text3 = styled.p`
+  margin: 0.8rem 0 0 0; 
+  font-size: 0.7rem;
+`
+
+const Text4 = styled.p`
+  margin: 3rem 0 0 0; 
+  color: #2F0047;
+  text-decoration: underline;
+  text-underline-offset: 0.3rem;
+`
