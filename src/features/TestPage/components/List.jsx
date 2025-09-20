394d36dd238d9a346a5dd076034c83930b62bcb8
choidaye8170 <gracechoidaye@gmail.com>
2025-09-20 23:50:00 +0900

    feat : 리스트에 배열 파일 적용

--- src/features/TestPage/components/List.jsx
+++ src/features/TestPage/components/List.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-import ListItem from './ListItem'
+import React from 'react';
+import styled from 'styled-components';
+import ListItem from './ListItem';
+import { survey } from "../components/SurveyQuestions"; 
 
-const List = () => {
+const List = ({ questionIndex }) => {
+  const currentQuestion = survey[questionIndex]; 
+  if (!currentQuestion) return null; 
+
   return (
     <Container>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        
+      {currentQuestion.options.map((option, idx) => (
+        <ListItem key={idx} content={option} />
+      ))}
     </Container>
-  )
-}
+  );
+};
 
 export default List;
 
-const Container=styled.div`
-    width: 70%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 1rem;
-`
+const Container = styled.div`
+  width: 85%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1rem;
+`;
