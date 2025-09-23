d97df5196c4075a21a0e604e41037eb6863f06a5
choidaye8170 <gracechoidaye@gmail.com>
2025-09-23 11:48:53 +0900

    design : serviceInfo 두번째 페이지 퍼블리싱 수정 완료

--- src/features/ServiceInfo/pages/SecondPage.jsx
+++ src/features/ServiceInfo/pages/SecondPage.jsx
@@ -4,6 +4,9 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import BackgroundImg from "../assets/Background.png";
 import TitleBgImg from "../assets/TitleBackground.png";
 import TextBgImg from "../assets/TalkIcon.png";
+import starIcon from "../assets/Star.png"
+import step1 from "../assets/Step1.png"
+import step2 from "../assets/Step2.png"
 
 const SecondPage = () => {
   const location = useLocation();
@@ -19,26 +22,22 @@ const SecondPage = () => {
   return (
     <Container onClick={handleNavigate}>
         <TitleWrapper>
-          <TitleBackground src={TitleBgImg} alt="title-bg" />
+          <Star src={starIcon}/>
           <Title>매일 작은 미션에 도전해요</Title>
         </TitleWrapper>
-        
-        <Text1>"매일 예상하지 못한 미션이 나와서<br />도전하는 재미가 있어요."</Text1>
-        <Content>서로의 미션 인증글에 응원을 남겨보세요</Content>
-        <TextWrapper>
-          <TextBackground src={TextBgImg} alt="text-bg" />
-          <Text2>
-            "나랑 비슷한 사람들이 있다는 사실 <br />
-            자체만으로도 위로가 됐어요."
-          </Text2>
-        </TextWrapper>
-
-        <Content>
-          미션인증으로 솜뭉치를 모아 <br />인근 가게의 할인쿠폰을 구입할 수 있어요.
-        </Content>
-
+        <ContentWrapper>
+            <Wrapper>
+              <Text>STEP1</Text>
+              <Icon1 src={step1}/>
+              <Content>미션 인증글에 응원을 남겨 보세요.</Content>
+            </Wrapper>
+            <Wrapper>
+              <Text>STEP1</Text>
+              <Icon2 src={step2}/>
+              <Content>미션 인증으로 솜뭉치를 모아 인근 가게의 <br/>할인 쿠폰을 구입해요.</Content>
+            </Wrapper>
+        </ContentWrapper>
         <Box>
-          {nickname}님의 레벨: {level} <br />
           한걸음씩, 이불 밖도 따뜻한 세상으로<br />
           나갈 습관을 함께 만들어 봐요!
         </Box>
@@ -52,91 +51,93 @@ export default SecondPage;
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  background: url(${BackgroundImg}) no-repeat center center;
+  background: linear-gradient(to bottom, #2F0047, #856b9b);
   background-size: cover;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  padding-top: 6rem;
-  padding-bottom: 7rem;
+  padding-top: 3.2rem;
+  padding-bottom: 1.8rem;
   padding-left: 2rem;
   padding-right: 2rem;
   cursor: pointer; /* ✅ 클릭 가능하게 */
 `;
 
 const TitleWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  margin-bottom: 1rem;
-  width: 17rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom:1rem;
 `;
 
-const TitleBackground = styled.img`
-  width: 100%;
-  height: auto;
-`;
+const Star=styled.img`
+  width: 1.4rem;
+`
+
 
 const Title = styled.p`
   margin: 0;
-  position: absolute;
   width: 100%;
-  top: 46%;
-  left: 58%;
-  transform: translate(-50%, -50%);
-  font-size: 1rem;
+  font-size: 1.2rem;
   text-align: center;
+  color: white;
 `;
 
-const Text1 = styled.p`
-  margin: 0;
-  font-size: 0.8rem;
-  text-align: right;
-  width: 100%;
-  color: #2F0047;
-`;
-
-const TextWrapper = styled.div`
-  position: relative;
+const ContentWrapper=styled.div`
   display: flex;
-  align-items: center;
-  justify-content: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
   width: 100%;
-  margin: 0;
-`;
+`
 
-const TextBackground = styled.img`
-  width: 13rem;
-  height: auto;
-`;
+const Wrapper=styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
 
-const Text2 = styled.p`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  margin: 0;
-  font-size: 0.8rem;
+const Text=styled.p`
+  margin: 0 0 0.8rem 0;
+  background: #D1CDFF;
+  padding: 0.4rem 0.7rem;
+  border-radius: 6px;
   color: #2F0047;
-  text-align: left;
-  width: 11rem;     
-`;
+  font-size: 0.8rem;
+  font-weight: 600;
+`
 
-const Content = styled.p`
-  margin: 0;
-  font-size: 1rem;
-  text-align: center;
-`;
+const Icon1=styled.img`
+  width: 11rem;
+`
+
+const Icon2=styled.img`
+  width: 8.5rem;
+`
+
+const Content=styled.p`
+  margin: 1rem 0;
+  color: white;
+  font-size: 0.8rem;
+  font-weight: 550;
+  text-align: start;
+`
+
 
 const Box = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 77%;
-  height: 4rem;
+  width: 85%;
+  height: 3.3rem;
+  margin-top: 1rem;
   background: #E4E0F6;
   border-radius: 30px;
   text-align: center;
-  font-weight: 500;
+  font-weight: 550;
   font-size: 0.9rem;
   color: #2F0047;
 `;
